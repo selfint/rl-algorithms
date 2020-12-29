8e4c200eff89c378ebb9d89351949f65ad31cfe2
Tom Selfin <selfint@gmail.com>
2020-12-29 22:57:43 +0200

    add act method

--- src/algorithms/q_learning.rs
+++ src/algorithms/q_learning.rs
@@ -1,12 +1,50 @@
-pub struct QLearner {
-    q_table: Vec<Vec<f32>>,
+use rand::{thread_rng, Rng};
+use std::{collections, hash::Hash};
+
+pub struct QLearner<S>
+where
+    S: Eq + Hash,
+{
+    q_table: collections::HashMap<S, Vec<f32>>,
+    actions: usize,
+    epsilon: f32,
+    epsilon_threshold: f32,
 }
 
-impl QLearner {
-    pub fn new(states: usize, actions: usize) -> Self {
+impl<S> QLearner<S>
+where
+    S: Eq + Hash,
+{
+    pub fn new(actions: usize) -> Self {
         Self {
-            q_table: vec![vec![0.; actions]; states],
+            q_table: collections::HashMap::new(),
+            actions,
+            epsilon: 1.,
+            epsilon_threshold: 0.1,
         }
+    }
+
+    pub fn act(&self, state: &S) -> usize {
+        if self.epsilon <= self.epsilon_threshold {
+            if let Some(action_values) = self.q_table.get(state) {
+                let max_action_value: &f32 = action_values
+                    .iter()
+                    .max_by(|&x, &y| {
+                        x.partial_cmp(y)
+                            .expect("Action values contained NaN - can't find max")
+                    })
+                    .expect("Failed to find max action value");
+                let best_action = action_values
+                    .iter()
+                    .position(|v: &f32| (v - max_action_value).abs() < f32::EPSILON)
+                    .unwrap();
+                return best_action;
+            } else {
+                return thread_rng().gen_range(0..self.actions);
+            }
+        }
+
+        thread_rng().gen_range(0..self.actions)
     }
 }
 
@@ -16,21 +54,14 @@ mod tests {
     use rl_environments::jump_environment::JumpEnvironment;
 
     #[test]
-    fn q_table_contains_all_state_action_pairs() {
-        let env = JumpEnvironment::new(10);
-
-        // states are all possible distances from the
-        // nearest wall, and if it is a high or low wall
-        let states = (env.size - env.player_col) * 2;
-
-        // jump or don't jump
-        let actions = 2;
-
-        let learner = QLearner::new(states, actions);
-
-        assert_eq!(learner.q_table.len(), states);
-        for state_action_values in learner.q_table {
-            assert_eq!(state_action_values.len(), actions);
+    fn test_act() {
+        let mut env = JumpEnvironment::new(10);
+        let learner = QLearner::new(2);
+        let env_state = env.simple_state().unwrap();
+        let action = learner.act(&env_state);
+        if let 1 = action {
+            env.jump()
         }
+        env.update();
     }
 }

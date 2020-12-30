bb643fbff05889f7b80a802e40534d45f460d646
Tom Selfin <selfint@gmail.com>
2020-12-31 00:07:19 +0200

    update example

--- examples/q_learner.rs
+++ examples/q_learner.rs
@@ -8,17 +8,17 @@ fn main() {
     let mut learner = QLearner::new(2, 1., 0.1);
 
     for e in 0..10 {
-        let mut env = JumpEnvironment::new(7);
+        let mut env = JumpEnvironment::new(10);
         let mut score: i128 = 0;
 
         while !env.done {
-            let state = &env.state.clone();
-            let action = learner.act(state);
+            let state = env.observe().clone();
+            let action = learner.act(&state);
             let reward = env.step(action);
             score += reward as i128;
 
-            let next_state = &env.state;
-            learner.learn(state, next_state, action, reward as f32);
+            let next_state = env.observe();
+            learner.learn(&state, next_state, action, reward as f32);
 
             // clear console and reset cursor
             print!("\x1B[2J\x1B[1;1H");

2b7c344a9b56a1f6837e4bb7b1bcd5e7b8e613e9
Tom Selfin <selfint@gmail.com>
2020-12-30 20:15:48 +0200

    rename example and delete old one

--- examples/q_learner.rs
+++ examples/q_learner.rs
@@ -4,39 +4,27 @@ use rl_environments::jump_environment::JumpEnvironment;
 use std::{thread, time};
 
 fn main() {
-    let mut learner = QLearner::new(2, 0.5, 0.1);
-    for e in 0..1000 {
-        let mut env = JumpEnvironment::new(10);
+    // notice that state type is detected automatically, in line 15
+    let mut learner = QLearner::new(2, 1., 0.1);
+
+    for e in 0..10 {
+        let mut env = JumpEnvironment::new(7);
         let mut score: i128 = 0;
 
         while !env.done {
-            if let Some(env_state) = env.simple_state() {
-                let action = learner.act(&env_state);
-                if let 1 = action {
-                    env.jump();
-                }
+            let state = &env.state.clone();
+            let action = learner.act(state);
+            let reward = env.step(action);
+            score += reward as i128;
 
-                let reward = env.update();
-                score += reward as i128;
+            let next_state = &env.state;
+            learner.learn(state, next_state, action, reward as f32);
 
-                if let Some(next_state) = env.simple_state() {
-                    learner.learn(&env_state, &next_state, action, reward as f32);
-                }
-            } else {
-                env.update();
-            }
+            // clear console and reset cursor
+            print!("\x1B[2J\x1B[1;1H");
 
-            if e > 950 || score > 10 {
-                // clear console and reset cursor
-                print!("\x1B[2J\x1B[1;1H");
-
-                println!("{}\nscore={} epoch={}", &env, score, e);
-                thread::sleep(time::Duration::from_millis(100));
-            }
-        }
-        if e > 950 {
-            println!("DEAD");
-            thread::sleep(time::Duration::from_millis(500));
+            println!("{}\nepoch={} score={}", &env, e, score);
+            thread::sleep(time::Duration::from_millis(100));
         }
     }
 }

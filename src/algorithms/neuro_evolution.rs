a0b4d2e5986e54d0d9b1e1a1248b96ff8217fca8
Tom Selfin <selfint@gmail.com>
2021-01-09 00:53:07 +0200

    update to version 0.2.0 of neuron

--- src/algorithms/neuro_evolution.rs
+++ src/algorithms/neuro_evolution.rs
@@ -2,23 +2,29 @@ use ndarray::prelude::*;
 use ndarray_stats::QuantileExt;
 use rand::Rng;
 
-use neuron::layers::FullyConnected;
+use neuron::layers::relu::ReLuLayer;
+use neuron::network::FeedForwardNetwork;
+use neuron::Layer;
+use rand::prelude::ThreadRng;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Agent {
-    network: FullyConnected,
+    network: FeedForwardNetwork<ReLuLayer>,
 }
 
 impl Agent {
     pub fn new(dims: &[usize]) -> Self {
         assert!(!dims.is_empty());
+        let layers: Vec<ReLuLayer> = (dims.iter().zip(dims.iter().skip(1)))
+            .map(|(&layer, &next_layer)| ReLuLayer::new(next_layer, layer))
+            .collect();
 
         Agent {
-            network: FullyConnected::network(dims),
+            network: FeedForwardNetwork::new(&layers),
         }
     }
 
-    pub fn act(&self, state: &[f32]) -> usize {
+    pub fn act(&self, state: &Array1<f32>) -> usize {
         self.network.predict(state).argmax().unwrap()
     }
 }
@@ -36,40 +42,17 @@ impl NeuroEvolution {
         }
     }
 
+    pub fn new_generation(&mut self, scores: &Vec<f32>) {}
+
     pub fn child(a1: &Agent, a2: &Agent) -> Agent {
         let mut rng = rand::thread_rng();
-        let mut child_weights = Vec::with_capacity(a1.network.get_shape().len() - 1);
-        let mut child_biases = Vec::with_capacity(a1.network.get_shape().len() - 1);
-        let a1_weights = a1.network.get_weights();
-        let a2_weights = a2.network.get_weights();
-        let a1_biases = a1.network.get_biases();
-        let a2_biases = a2.network.get_biases();
-        let shape = a1.network.get_shape();
 
-        for i in 0..shape.len() - 1 {
-            let mut layer_weights = Array2::default((shape[i + 1], shape[i]));
-            layer_weights.assign(a1_weights[i]);
-            let mut layer_biases = Array1::default(shape[i + 1]);
-            layer_biases.assign(a1_biases[i]);
-
-            for j in 0..shape[i + 1] {
-                if rng.gen_bool(0.5) {
-                    layer_biases[j] = a2_biases[i][j];
-                }
-                for k in 0..shape[i] {
-                    if rng.gen_bool(0.5) {
-                        let weight = layer_weights.get_mut((j, k)).unwrap();
-                        let a2_layer_weights = a2_weights[i];
-                        *weight = *a2_layer_weights.get((j, k)).unwrap();
-                    }
-                }
-            }
-            child_weights.push(layer_weights);
-            child_biases.push(layer_biases);
-        }
+        let mut child_network = a1.network.clone();
+        NeuroEvolution::crossover_weights(&mut child_network, a2, &mut rng);
+        NeuroEvolution::crossover_biases(&mut child_network, a2, &mut rng);
 
         Agent {
-            network: FullyConnected::build_stack(&child_weights, &child_biases),
+            network: child_network,
         }
     }
 
@@ -82,25 +65,63 @@ impl NeuroEvolution {
         }
     }
 
+    fn crossover_weights(
+        child_network: &mut FeedForwardNetwork<ReLuLayer>,
+        a2: &Agent,
+        rng: &mut ThreadRng,
+    ) {
+        for (child_weights, a2_weights) in child_network
+            .layers
+            .iter_mut()
+            .map(|l| l.get_weights_mut())
+            .zip(a2.network.layers.iter().map(|l| l.get_weights()))
+        {
+            for i in 0..child_weights.nrows() {
+                for j in 0..child_weights.ncols() {
+                    if rng.gen_bool(0.5) {
+                        child_weights[[i, j]] = a2_weights[[i, j]];
+                    }
+                }
+            }
+        }
+    }
+
+    fn crossover_biases(
+        child_network: &mut FeedForwardNetwork<ReLuLayer>,
+        a2: &Agent,
+        rng: &mut ThreadRng,
+    ) {
+        for (child_biases, a2_biases) in child_network
+            .layers
+            .iter_mut()
+            .map(|l| l.get_biases_mut())
+            .zip(a2.network.layers.iter().map(|l| l.get_biases()))
+        {
+            for i in 0..child_biases.len() {
+                if rng.gen_bool(0.5) {
+                    child_biases[i] = a2_biases[i];
+                }
+            }
+        }
+    }
+
     fn mutate_weight(agent: &mut Agent) {
         let mut rng = rand::thread_rng();
-        let mut network_weights = agent.network.get_weights_mut();
-        let layer = rng.gen_range(0..network_weights.len() - 1);
-        let node = rng.gen_range(0..network_weights[layer].shape()[0]);
-        let previous_node = rng.gen_range(0..network_weights[layer].shape()[1]);
-        let weight = network_weights[layer]
-            .get_mut([node, previous_node])
-            .unwrap();
+        let layer = rng.gen_range(0..agent.network.layers.len() - 1);
+        let layer_weights = agent.network.layers[layer].get_weights_mut();
+        let node = rng.gen_range(0..layer_weights.nrows());
+        let previous_node = rng.gen_range(0..layer_weights.ncols());
+        let weight = layer_weights.get_mut([node, previous_node]).unwrap();
 
         *weight = rng.gen_range(-0.01..0.01);
     }
 
     fn mutate_bias(agent: &mut Agent) {
         let mut rng = rand::thread_rng();
-        let mut network_biases = agent.network.get_biases_mut();
-        let layer = rng.gen_range(0..network_biases.len() - 1);
-        let node = rng.gen_range(0..network_biases[layer].shape()[0]);
-        let bias = network_biases[layer].get_mut(node).unwrap();
+        let layer = rng.gen_range(0..agent.network.layers.len() - 1);
+        let layer_biases = agent.network.layers[layer].get_biases_mut();
+        let node = rng.gen_range(0..layer_biases.len());
+        let bias = layer_biases.get_mut(node).unwrap();
 
         *bias = rng.gen_range(-0.01..0.01);
     }
@@ -109,23 +130,48 @@ impl NeuroEvolution {
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn test_new_generation() {
-        let neuro_evolution = NeuroEvolution::new(10, &[2, 3, 1]);
+        let mut neuro_evolution = NeuroEvolution::new(10, &[2, 3, 1]);
+        let first_generation = neuro_evolution.population.clone();
         assert_eq!(neuro_evolution.population.len(), 10);
+
+        // score population
+        let mut rng = rand::thread_rng();
+        let scores = (0..10).map(|_| rng.gen_range(-2.0..10.0)).collect();
+        neuro_evolution.new_generation(&scores);
     }
 
     #[test]
     fn test_mutate() {
         let mut a = Agent::new(&[2, 3, 1]);
-        let network_weights_before = a.network.clone_weights();
-        let network_biases_before = a.network.clone_biases();
+        let network_weights_before: Vec<Array2<f32>> = a
+            .network
+            .layers
+            .iter()
+            .map(|l| l.get_weights().clone())
+            .collect();
+        let network_biases_before: Vec<Array1<f32>> = a
+            .network
+            .layers
+            .iter()
+            .map(|l| l.get_biases().clone())
+            .collect();
 
         NeuroEvolution::mutate(&mut a);
 
-        let network_weights_after = a.network.clone_weights();
-        let network_biases_after = a.network.clone_biases();
+        let network_weights_after: Vec<Array2<f32>> = a
+            .network
+            .layers
+            .iter()
+            .map(|l| l.get_weights().clone())
+            .collect();
+        let network_biases_after: Vec<Array1<f32>> = a
+            .network
+            .layers
+            .iter()
+            .map(|l| l.get_biases().clone())
+            .collect();
 
         assert!(
             network_weights_before != network_weights_after
@@ -141,13 +187,13 @@ mod tests {
 
         assert_ne!(child.network, a1.network);
         assert_ne!(child.network, a2.network);
-        assert_eq!(child.network.get_shape(), a1.network.get_shape());
+        assert_eq!(child.network.shape(), a1.network.shape());
     }
 
     #[test]
     fn test_agent_action() {
         let agent = Agent::new(&[2, 3, 1]);
-        let state = [0., 1.];
+        let state = arr1(&[0., 1.]);
         let actions = 2;
         let agent_action = agent.act(&state);
 
